8e568fdcf5c7b27510dba59060afbd3a2dd4eaf4
haiktpm <haitv2809@gmail.com>
2026-01-29 01:29:07 +0700

    Remove db connection

--- index.js
+++ index.js
@@ -1,23 +1,5 @@
 const express = require('express')
 const app = express()
-const { Pool } = require('pg')
-
-const pool = new Pool({
-  user: process.env.pg_user,
-  database: process.env.pg_database,
-  password: process.env.pg_password,
-  port: process.env.pg_port,
-  host: process.env.pg_host,
-})
-
-const createUser = (request, response) => {
-  pool.query('INSERT INTO public.user_access (user_info) VALUES ($1)', [request.headers['user-agent']], (error, results) => {
-    if (error) {
-      throw error
-    }
-  })
-}
-
 const port = process.env.PORT || 3032;
 var os = require('os');
 var d = new Date();
@@ -26,7 +8,6 @@ var responsev2 = responsev1 + "<br/>Hello Bestarion - " + `${d.getDate()}/${d.ge
 var responsev3 = responsev2 + "<br/>System info:<br/>CPU: " + os.loadavg() + "  Memory total: " + os.totalmem() + " Free memory: " + os.freemem()
 app.get('/', (req, res) => {
   console.log(`${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}  -  ${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}` + "-----" + req.headers['user-agent'])
-  createUser(req, res)
   res.send(responsev2)
 })
 app.listen(port, (err) => {
